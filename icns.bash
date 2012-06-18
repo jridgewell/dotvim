be17430c322e80af738b94b7c9aa343d629cdbdf
Justin Ridgewell <justin@ridgewell.name>
2012-06-18 01:05:32 -0400

    Have icns.bash work from where ever it's run from

--- icns.bash
+++ icns.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mvi=`readlink ~/Applications/MacVim.app`
-cp "MacVim.icns" "${mvi}/Contents/Resources/MacVim.icns"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp "${script_dir}/MacVim.icns" "${mvi}/Contents/Resources/MacVim.icns"
